25517d32b0fa1bd9d872102eb255879568f22cd2
nanabrice <byce.0002@gmail.com>
2023-07-06 10:35:00 +0100

    notification done

--- client/src/pages/Project_pages/UpdateProject.js
+++ client/src/pages/Project_pages/UpdateProject.js
@@ -1,6 +1,5 @@
 import "./UpdateProject.css";
 
-import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
